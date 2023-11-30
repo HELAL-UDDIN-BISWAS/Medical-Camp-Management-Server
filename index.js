9534570bd033354c5791b9441725d3850f76b4c6
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2023-11-30 19:28:03 +0600

    code added home page card

--- index.js
+++ index.js
@@ -266,6 +266,13 @@ async function run() {
       const result = await ParticipantCamps.deleteOne(query)
       res.send(result)
     })
+    
+    app.delete('/deletecamp/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await availableCamps.deleteOne(query)
+      res.send(result)
+    })
   
 
 
